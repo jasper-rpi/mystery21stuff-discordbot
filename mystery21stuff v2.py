db9e3b72ebae05227d943a3ee3bd4fd8dee8e3bc
jasper-rpi <97444973+jasper-rpi@users.noreply.github.com>
2023-04-15 18:47:06 +0300

    Updated the laused file structure
    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- mystery21stuff v2.py
+++ mystery21stuff v2.py
@@ -5,12 +5,13 @@ import discord
 from discord.ext import commands
 import asyncio
 import ast
+import json
 
 
 
 intents = discord.Intents.all()
 
-bot = commands.Bot(command_prefix="$", intents=intents)
+bot = commands.Bot(command_prefix="$", intents=intents, activity = discord.Game(name = 'your great-grandfather'))
 
 opentoken = open("token.txt", 'r')
 TOKEN = opentoken.read()
@@ -28,7 +29,11 @@ async def get_message_history(user_id, channel):
     async for message in channel.history(limit=200):
         if message.author == user or message.author == bot.user:
             await channel.send(f'{message.created_at}: {message.author}: {message.content}')
-
+            
+@bot.command(help='shutdownib boti')
+@commands.is_owner()
+async def shutdown(ctx):
+    exit()
 
 @bot.event
 async def on_ready():
@@ -43,6 +48,22 @@ async def on_message(message):
     if message.author == bot.user:
         return
     
+
+    if "ekre" in message.content:
+        ekre_open = open('EKRE.txt', 'r')
+        ekre_read = ekre_open.read()
+        await message.channel.send(ekre_read)
+        ekre_open.close()
+        return
+    if "EKRE" in message.content:
+        ekre_open = open('EKRE.txt', 'r')
+        ekre_read = ekre_open.read()
+        await message.channel.send(ekre_read)
+        ekre_open.close()
+        return
+    if "lappa kotte" in message.content:
+        await message.channel.send("ma ei lappa kotte")
+        return
     if not message.content.startswith(bot.command_prefix): # check if the message is not a command
         rest_channels = open(rest_chan, 'r+')
         restricted_channels = rest_channels.read().splitlines()
@@ -50,29 +71,23 @@ async def on_message(message):
             return  # Exit the function without sending a ctx
 
         # If the ctx is not in a restricted channel, continue with normal behavior
-        filee = open(laused, 'r')
-        lause = filee.read().splitlines()
-        await message.channel.send(random.choice(lause))
-        filee.close()
+        with open("laused.txt") as f:
+            data = json.load(f)
+
+        random_lause = random.choice([d["lause"] for d in data])
+
+        await message.channel.send(random_lause)
+
         rest_channels.close()
-    elif "ekre" in message.content:
-        ekre_open = open('EKRE.txt', 'r')
-        ekre_read = ekre_open.read()
-        print(ekre_read)
-        await message.channel.send(ekre_read)
-        ekre_open.close()
-    elif "EKRE" in message.content:
-        ekre_open = open('EKRE.txt', 'r')
-        ekre_read = ekre_open.read()
-        print(ekre_read)
-        await message.channel.send(ekre_read)
-        ekre_open.close()
     else:
-        await bot.process_commands(message)
+        try:
+            await bot.process_commands(message)
+        except Exception as e:
+            await message.channel.send("""vale command tumba.
+Error: {e}""")
     
 
-@bot.command()
-
+@bot.command(help='teeb matemaatikat')
 async def math(ctx, ekvatsioon: str):
     await ctx.channel.send('mathing:nerd:')
     await asyncio.sleep(1)
@@ -97,7 +112,7 @@ async def math(ctx, ekvatsioon: str):
     await ctx.channel.send(tehet)
     
     
-@bot.command()
+@bot.command(help='lisab scoori')
 async def score(ctx, mesag_con: int):
     file = 'scoor.txt'
     openscoor = open(file,"r+")
@@ -112,45 +127,57 @@ async def score(ctx, mesag_con: int):
     opnscr.close()
     
     
-@bot.command()
+@bot.command(help=':skull:')
 async def meth(ctx):
     await ctx.channel.send('mething :100:')
     await asyncio.sleep(1)
     await ctx.channel.send(':100: :100: :100: :100: :100: :100: :100: :100: :100: :100: :100: :100: :100: :100: :100: :100: :100: :100: :100: :100:')
     
     
-@bot.command()
+@bot.command(help='genereerib nitrot')
 async def nitrogen(ctx, nitro: int):
     await ctx.channel.send("*generating free nitro*")
     def gen_gift(length):
         return secrets.token_hex(length // 2)
 
+    if nitro <= 200:
+        for j in range(nitro):
+            for i in range(25):
+                GIFT_CODE = ('discord.gift/' + gen_gift(16))
+                
+                response = requests.get(f'https://discord.com/api/v6/entitlements/gift-codes/{GIFT_CODE}', headers={'Authorization': f'Bot {TOKEN}'})
+                # Check the status code of the response
+                if response.status_code == 200:
+                    # If the status code is 200, the gift code is valid
+                    await ctx.channel.send('Gift code is valid:   ' + 'discord.gift/' + '{GIFT_CODE}')
+                    break
+            await ctx.channel.send("didn't find nitro")
+            await asyncio.sleep(1)
+    else:
+        await ctx.channel.send("ei tee nii palju nitrot")
+            
+@bot.command(help='lisab suvalistesse lausetesse lause, mis sina kirjutasid')     
+async def addlause(ctx, *, lause):
+#     fail = open(laused, 'a')
+#     
+#     fail.write("""
+# """+ lause)
+#     
+#     fail.close()
 
-    for j in range(nitro):
-        for i in range(50):
-            GIFT_CODE = ('discord.gift/' + gen_gift(16))
-            
-            response = requests.get(f'https://discord.com/api/v6/entitlements/gift-codes/{GIFT_CODE}', headers={'Authorization': f'Bot {TOKEN}'})
-            # Check the status code of the response
-            if response.status_code == 200:
-                # If the status code is 200, the gift code is valid
-                await ctx.channel.send('Gift code is valid:   ' + 'discord.gift/' + '{GIFT_CODE}')
-                break
-        await ctx.channel.send("didn't find nitro")
-        await asyncio.sleep(1)
-            
-@bot.command()     
-async def addlause(ctx, lause: str):
-    fail = open(laused, 'a')
-    
-    fail.write("""
-"""+ lause)
-    
-    fail.close()
+    # Load existing lauses from the JSON file
+    with open('laused.txt', 'r') as f:
+        lauses = json.load(f)
+
+    # Add the new lause to the list
+    lauses.append({"lause": lause})
+
+    # Save the updated list of lauses to the JSON file
+    with open('laused.txt', 'w') as f:
+        json.dump(lauses, f)
     await ctx.channel.send('lause "' + lause + '" on salvestatud')
     
-    
-@bot.command()
+@bot.command(help='kaheksapall annab vastuseid')
 async def kaheksapall(ctx, kusimus: str):
     fileee = open(kaheksaball, 'r')
     lausee = fileee.read().splitlines()
@@ -158,7 +185,7 @@ async def kaheksapall(ctx, kusimus: str):
     fileee.close()
     
     
-@bot.command()
+@bot.command(help='teeb posu nooli')
 async def nool(ctx, noolarv: int):
     fliec = open(noolfail, 'w')
     fliec.close()
@@ -177,7 +204,7 @@ async def nool(ctx, noolarv: int):
         filew.close()
         
         
-@bot.command()
+@bot.command(help='kasutu')
 async def commandid(ctx):
     await ctx.channel.send("""Boti prefiks on $
 Commandid:
@@ -188,11 +215,12 @@ meth - :skull:
 8pall - see annab vastuse
 score - lisab antud koguse skoori uleuldisesse skoori kogusesse
 addlause - lisab lause
+invite - 
 """)
         
         
-@bot.command()
-async def removebot(ctx):
+@bot.command(help='ei lase botil saata suvalisi lauseid selles kanalis')
+async def shutup(ctx):
     restr_channels = open(rest_chan, 'a+')
     restr_channels.write(str(ctx.channel.id) + '''
 ''')
@@ -203,11 +231,16 @@ async def removebot(ctx):
     print(restricted_channels)
     
     
-@bot.command()
-async def history(ctx, user_id: str):
-    channel = ctx.channel
-    await get_message_history(user_id, channel)
+# @bot.command()
+# async def history(ctx, user_id: str):
+#     channel = ctx.channel
+#     await get_message_history(user_id, channel)
+    
+@bot.command(help='teeb invite lingi botile')
+async def invite(ctx):
+    await ctx.channel.send("""Siin on invite:
+https://discord.com/api/oauth2/authorize?client_id=1058116264628850768&permissions=8&scope=bot""")
         
-     
-
+        
+    
 bot.run(TOKEN)
